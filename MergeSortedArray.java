2790edd7a6b1e7b17ee31172bbed34a0100896b9
msk00007 <109849322+msk00007@users.noreply.github.com>
2026-01-15 16:12:26 +0530

    Add merge function for two sorted arrays
    Implement merging of two sorted arrays into nums1.

---
+++ MergeSortedArray.java
@@ -0,0 +1,23 @@
+ int i = m - 1;          // last valid element in nums1
+        int j = n - 1;          // last element in nums2
+        int k = m + n - 1;      // merge position
+
+        // Merge from the back
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // Copy remaining elements of nums2 (if any)
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
